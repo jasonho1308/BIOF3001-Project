b26b2f5b6c1c1208f1aabd4e91c32001f0647f1f
Ho Cheuk Hai Jason <50993239+jasonho1308@users.noreply.github.com>
2025-10-23 04:46:06 +0800

    read barcodes from file

--- scripts/epigenetic_clock.R
+++ scripts/epigenetic_clock.R
@@ -1,36 +1,35 @@
-barcodes_small <- c("TCGA-E9-A1N8-11A", "TCGA-E2-A1BC-11A", "TCGA-E9-A1ND-11A", "TCGA-AC-A2FM-11B")
+barcodes <- readLines("data/processed/tcga_brca_barcodes.txt")
 
-# This line searches in the database and builds the query, selecting the beta values data from the TCGA project, pulling only the files for the 4 samples. This will take 2-3 minutes
+# Query metadata for selected barcodes
 query_TCGA_meth <- GDCquery(
   project       = "TCGA-BRCA",
   data.category = "DNA Methylation",
   data.type     = "Methylation Beta Value",
-  barcode       = barcodes_small # <— restrict to just these 4 samples
+  barcode       = barcodes
 )
 
-# To extract and view the metadata of the data we'll be downloading:
+# View query results
 res_TCGA_meth <- getResults(query_TCGA_meth)
 View(res_TCGA_meth) # metadata
 
-# To download the actual data files from the GDC based on the previously constructed query:
+# Download actual data
 GDCdownload(query_TCGA_meth, directory = "data/raw/GDCdata/")
 me_TCGA <- GDCprepare(query_TCGA_meth, directory = "data/raw/GDCdata/")
-dim(me_TCGA) # methylation x 4 samples
+dim(me_TCGA) 
 
 # prepare data for methylclock
 
-data <- assay(me_TCGA)
+data <- assay(me_TCGA) # converts to useable matrix
 data <- na.omit(data)
 samples <- colnames(data)
 
-# ordered.metadata <- metadata[match(samples, metadata$Sample), ]
 age <- me_TCGA$age_at_index
 
 # checks for missing CpGs for the various clocks
-cpgs.missing <- checkClocks(data) # approx 1 min to run
+cpgs.missing <- checkClocks(data) 
 
 # makes prediction based on methylation data of normal samples
-prediction <- DNAmAge(data, age = age, cell.count = FALSE) # approx <2 mins to run
+prediction <- DNAmAge(data, age = age, cell.count = FALSE)
 
 # Scatter plots with regression lines using tidy evaluation
 chron_age <- prediction$age
