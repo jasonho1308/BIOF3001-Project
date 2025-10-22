9092418d28d40e7834117fcecab5977d325040c9
Ho Cheuk Hai Jason <50993239+jasonho1308@users.noreply.github.com>
2025-10-23 04:36:03 +0800

    Add initial library setup and epigenetic clock analysis scripts

---
+++ library.R
@@ -0,0 +1,52 @@
+# load required libraries
+packages <- c("BiocManager", "data.table", "tidyverse", "dplyr", "ggplot2", "patchwork", "DT", "tidyr")
+not_installed <- setdiff(packages, rownames(installed.packages()))
+if (length(not_installed)) install.packages(not_installed)
+lapply(packages, library, character.only = TRUE)
+
+if (!require("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+}
+BiocManager::install(version = "3.21")
+
+if (!requireNamespace("DESeq2", quietly = TRUE)) {
+    BiocManager::install("DESeq2")
+}
+library(DESeq2)
+
+if (!requireNamespace("methylclock", quietly = TRUE)) {
+    BiocManager::install("methylclock")
+    BiocManager::install("methylclockData")
+}
+library(methylclock)
+library(methylclockData)
+
+
+if (!requireNamespace("clusterProfiler", quietly = TRUE)) {
+    BiocManager::install("clusterProfiler")
+}
+library(clusterProfiler)
+if (!requireNamespace("msigdbr", quietly = TRUE)) {
+    BiocManager::install("msigdbr")
+}
+library(msigdbr)
+if (!requireNamespace("org.Hs.eg.db", quietly = TRUE)) {
+    BiocManager::install("org.Hs.eg.db")
+}
+library(org.Hs.eg.db)
+if (!requireNamespace("enrichplot", quietly = TRUE)) {
+    BiocManager::install("enrichplot")
+}
+library(enrichplot)
+if (!requireNamespace("sesame", quietly = TRUE)) {
+    BiocManager::install("sesame")
+}
+library(sesame)
+if (!requireNamespace("sesameData", quietly = TRUE)) {
+    BiocManager::install("sesameData")
+}
+library(sesameData)
+if (!requireNamespace("TCGAbiolinks", quietly = TRUE)) BiocManager::install("TCGAbiolinks")
+if (!requireNamespace("SummarizedExperiment", quietly = TRUE)) BiocManager::install("SummarizedExperiment")
+library(TCGAbiolinks)
+library(SummarizedExperiment)
---
+++ scripts/epigenetic_clock.R
@@ -0,0 +1,49 @@
+barcodes_small <- c("TCGA-E9-A1N8-11A", "TCGA-E2-A1BC-11A", "TCGA-E9-A1ND-11A", "TCGA-AC-A2FM-11B")
+
+# This line searches in the database and builds the query, selecting the beta values data from the TCGA project, pulling only the files for the 4 samples. This will take 2-3 minutes
+query_TCGA_meth <- GDCquery(
+    project       = "TCGA-BRCA",
+    data.category = "DNA Methylation",
+    data.type     = "Methylation Beta Value",
+    barcode       = barcodes_small # <— restrict to just these 4 samples
+)
+
+# To extract and view the metadata of the data we'll be downloading:
+res_TCGA_meth <- getResults(query_TCGA_meth)
+View(res_TCGA_meth) # metadata
+
+# To download the actual data files from the GDC based on the previously constructed query:
+GDCdownload(query_TCGA_meth, directory = "data/raw/GDCdata/")
+me_TCGA <- GDCprepare(query_TCGA_meth, directory = "data/raw/GDCdata/")
+dim(me_TCGA) # methylation x 4 samples
+
+# prepare data for methylclock
+
+data <- assay(me_TCGA)
+data <- na.omit(data)
+samples <- colnames(data)
+
+# ordered.metadata <- metadata[match(samples, metadata$Sample), ]
+age <- me_TCGA$age_at_index
+
+# checks for missing CpGs for the various clocks
+cpgs.missing <- checkClocks(data) # approx 1 min to run
+
+# makes prediction based on methylation data of normal samples
+prediction <- DNAmAge(data, age = age, cell.count = FALSE) # approx <2 mins to run
+
+# Scatter plots with regression lines using tidy evaluation
+chron_age <- prediction$age
+clock_cols <- colnames(prediction[, c(2, 5, 8, 12, 15, 18, 21, 24, 27)])
+
+scatter_plots <- lapply(clock_cols, function(clock) {
+    ggplot(prediction, aes(x = age, y = !!sym(clock))) +
+        geom_point(alpha = 0.5) +
+        geom_smooth(method = "lm", color = "red") +
+        labs(title = clock, x = "Chronological Age", y = "Predicted DNAmAge") +
+        theme_minimal()
+})
+
+scatter_grid <- wrap_plots(scatter_plots, ncol = 3)
+
+print(scatter_grid)
